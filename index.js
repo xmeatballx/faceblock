b0a6d6e02097a23c090de7b2033398a1ca18dca8
xmeatballx <erik.jensen5@pcc.edu>
2020-06-03 00:43:25 -0700

    first deploy commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(3000)
+
+const multer = require('multer')
+const path = require('path')
+var storage = multer.diskStorage({
+	destination: function(req, file, cd) {
+		cd(null, "public")
+	},
+	filename: function (req, file, cb) {
+		cb(null, 'image.jpg')
+	}
+})
+
+var upload = multer({storage: storage})
+
+app.post('/upload', upload.single('photo'), (req, res, err) => {
+	console.log(err)
+})
---
+++ public/sketch.js
@@ -0,0 +1,76 @@
+var imgjs;
+var imghtml;
+var detections;
+var submit;
+var slider;
+var boxPos;
+var boxDim;
+var boxes = [];
+var uploaded;
+
+function setup() {
+	createCanvas(0, 0);
+	boxDim = createVector(0, 0);
+	boxPos = createVector(0, 0);
+	imgjs = loadImage("image.jpg");
+	imghtml = document.getElementById("img");
+	submit = document.getElementById("submit");
+	submit.addEventListener("click", assignSrc);
+	uploaded = false;
+}
+
+function assignSrc() {
+	background(255);
+	imghtml.src = "image.jpg";
+	imgjs = loadImage("image.jpg", () => {
+		uploaded = true;
+	});
+}
+
+function draw() {
+	background(255);
+	resizeCanvas(imgjs.width, imgjs.height);
+	image(imgjs, 0, 0);
+	for (var i = 0; i < boxes.length; i++) {
+		if (boxes[i]) boxes[i].display();
+	}
+}
+
+Promise.all([faceapi.nets.ssdMobilenetv1.loadFromUri("/models")]).then(detect);
+
+function detect() {
+	setInterval(async () => {
+		detections = await faceapi.detectAllFaces(
+			document.getElementById("img"),
+			new faceapi.SsdMobilenetv1Options({ minConfidence: 0.3 })
+		);
+		if (uploaded == false && imgjs) {
+			for (var i = 0; i < detections.length; i++) {
+				boxPos.x = detections[i].box.x;
+				boxPos.y = detections[i].box.y;
+				boxDim.x = detections[i].box.width;
+				boxDim.y = detections[i].box.height;
+				boxes[i] = new Box(boxPos.x, boxPos.y, boxDim.x, boxDim.y);
+			}
+		} else {
+			boxes.length = 0;
+			uploaded = false;
+		}
+		//drawBox(boxPos.x, boxPos.y, boxDim.x, boxDim.y)
+		//console.log(detections)
+	}, 100);
+}
+
+class Box {
+	constructor(x, y, w, h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+
+	display() {
+		fill(0);
+		rect(this.x, this.y, this.w, this.h);
+	}
+}
