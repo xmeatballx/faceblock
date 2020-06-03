335c80998b96fc3ef4409857a2916969f388fa1c
xmeatballx <erik.jensen5@pcc.edu>
2020-06-03 00:54:51 -0700

    fixed issue in html img src

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 
 const multer = require("multer");
 const path = require("path");
